eea4d1e59dd18f8b01ab05dfbc8fd3f15e004274
sachrulakbar21 <147585421+sachrulakbar21@users.noreply.github.com>
2025-06-03 17:17:58 +0700

    Delete src/petemuan14 directory

--- src/petemuan14/tugas_4/TestDatabaseConnection.java
+++
@@ -1,9 +0,0 @@
-package tugas_4;
-
-public class TestDatabaseConnection {
-    public static void main(String[] args) {
-        // Saat pertama kali mengakses DatabaseConnection.connectionString,
-        // static block di DatabaseConnection sudah otomatis dijalankan.
-        System.out.println("Nilai connectionString: " + DatabaseConnection.connectionString);
-    }
-}
